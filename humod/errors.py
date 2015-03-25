839a85beeec05e3bfefee0047ff4e322f1a1f6e9
F Malina <fmalina@gmail.com>
2015-03-25 11:51:00 +0000

    Python3 exception.

--- humod/errors.py
+++ humod/errors.py
@@ -21,6 +21,4 @@ class HumodUsageError(Error):
 def check_for_errors(input_line):
     """Check if input line contains error code."""
     if ('ERROR' in input_line) or (input_line in ERROR_CODES):
-        raise AtCommandError, input_line
-
-
+        raise AtCommandError(input_line)
